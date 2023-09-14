7e2f2a70926cf523a6655b5d0a12a32bd33545eb
Walter N <2760walter@gmail.com>
2023-09-14 22:50:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase(string));
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === string.toUpperCase()){
+        return "YES INDEED!";
+    }
+    if(string === string.toLowerCase()){
+        return "I can't hear you!";
+    }
+    if(string==="Let's have dinner together!"){
+        return "I would love to!";
+    }
+}
+
+console.log(sayHiToHeadphonedRoommate("Let's have dinner together!"));
+console.log(sayHiToHeadphonedRoommate("lower!"));
+console.log(sayHiToHeadphonedRoommate("TABLE!"));
